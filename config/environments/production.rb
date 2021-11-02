220e92201d26d16637f4216961a1df470b1a3959
yu-skp0518 <reality_0518@yahoo.co.jp>
2021-11-02 16:50:36 +0000

    Tweak

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # 週毎
+  config.logger = Logger.new('log/development.log', 'weekly')
 end
