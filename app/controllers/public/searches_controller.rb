c2d916d8da21b99a0a1f86907cdc433119a12913
yu-skp0518 <reality_0518@yahoo.co.jp>
2021-11-27 15:20:33 +0000

    [add] search function(in the middle)

---
+++ app/controllers/public/searches_controller.rb
@@ -0,0 +1,57 @@
+class Public::SearchesController < ApplicationController
+
+  # def search
+  #     @content = params[search][content]
+  #     @how = params[search][how]
+  #     @model = params[search][model]
+  #     @datas = search_for(@how, @model, @content)
+  # end
+
+private
+
+  def match(model, content)
+    if model == 'user'
+      User.where(name: content)
+    elsif model == 'book'
+      Book.where(title: content)
+    end
+  end
+
+  def forward(model, content)
+    if model == 'user'
+      User.where('nick_name LIKE ?', "#{content}%")
+    elsif model == 'book'
+      Book.where('title LIKE ?', "#{content}%")
+    end
+  end
+
+  def backward(model, content)
+    if model == 'user'
+      User.where('nick_name LIKE ?', "%#{content}")
+    elsif model == 'book'
+      Book.where('title LIKE ?', "%#{content}")
+    end
+  end
+
+  def partical(model, content)
+    if model == 'user'
+      User.where('nick_name LIKE ?', "%#{content}%")
+    elsif model == 'book'
+      Book.where('title LIKE ?', "%#{content}%")
+    end
+  end
+
+  def search_for(how, model, content)
+    case how
+    when 'match'
+      match(model, content)
+    when 'forward'
+      forward(model, content)
+    when 'backward'
+      backward(model, content)
+    when 'partical'
+      partical(model, content)
+    end
+  end
+
+end
